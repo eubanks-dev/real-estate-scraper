74e3a12794a15e8cdd1d03495bd6d87eec691d1c
Jeff Eubanks <jeffeubanks@jeffs-mbp.lan>
2023-03-29 15:50:42 -0400

    Add more property data to the table, including city, state, and occupancy

--- scraper.py
+++ scraper.py
@@ -8,14 +8,19 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/scraped-data")
+@app.route("/parsed-realpage-data")
 def index():
+	"""This route displays scraped/parsed data from real estate data sources
+
+	Returns:
+		Jinja2 Template: A rendered HTML page dispalying a table of real estate property data
+	"""
 	relevant_data = parse_realpage()	
-	# print(relevant_data, file=sys.stderr)
 
 	'''
-	This template creates an HTML table with two columns: "Property Name" and "Unit Count". 
-	It iterates over the dictionary passed in as "table_data", and generates a new row in the table for each property.
+	This template creates an HTML page with a table of property data. 
+	It iterates over the list of dictionaries passed in as "table_data", and 
+	generates a new row in the table for each property in the list.
 	'''
 	return render_template('properties_table_template.html', table_data=relevant_data)
 
@@ -25,23 +30,29 @@ def parse_realpage():
 	Returns:
 		dictionary: dictionary containing relevant fields from Realpage property data
 	"""
-	realpage_data = hardcoded_realpage_data()
-	property_dict = {}
+	realpage_data = locally_stored_realpage_data()
+	property_list = []
 
 	# for each property in the realpage data
 	for property in realpage_data:
+		# parse relevant property data and store in dictionary
+		property_dict = {}
+		property_dict["name"] = property['propertyName']
+		property_dict["city"] = property['city']
+		property_dict["state"] = property['state']
+		property_dict["unitcount"] = property['unitcount']
 
-		# get property name and unit count from JSON data
-		property_name = property['propertyName']
-		unit_count = property['unitcount']
+		for metric_data in property['metricData']:
+			if metric_data['metricCode'] == 'MPF-OCC':
+				property_dict["occupancy"] = metric_data['metricValueFormatted']
 
-		# Add property name and unit count to dictionary
-		property_dict[property_name] = unit_count
+		# Add dictionary to list of properties
+		property_list.append(property_dict)
 
-	return property_dict
+	return property_list
 
 
-def hardcoded_realpage_data():
+def locally_stored_realpage_data():
 	"""Returns a locally stored copy of the JSON data returned by realpage at the following URL: https://dac.realpage.com/dac/markets/16980/properties
 
 	Returns:
